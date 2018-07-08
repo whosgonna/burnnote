8bd2d3153f3efd3e080647eff37e65e9b2f0618e
Ben Kaufman <ben.whosgonna.com@gmail.com>
2018-07-07 21:10:25 -0500

    created schema, worked on index page

--- lib/burnnote.pm
+++ lib/burnnote.pm
@@ -1,10 +1,24 @@
 package burnnote;
 use Dancer2;
+use Dancer2::Plugin::DBIC;
+use Data::Printer;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index' => { 'title' => 'burnnote' };
 };
+
+post '/' => sub {
+    my $body = body_parameters->as_hashref;
+    p $body;
+    validate_input();
+    template 'index' => { 'title' => 'burnnote' };
+};
+
+
+sub validate_input {
+};
+
 
 true;
