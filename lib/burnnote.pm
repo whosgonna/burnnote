6ef2f20edda7c3c8b5c60d2a402aff1e346334e3
Ben Kaufman <ben.whosgonna.com@gmail.com>
2018-07-07 22:02:33 -0500

    updated schema

--- lib/burnnote.pm
+++ lib/burnnote.pm
@@ -1,7 +1,12 @@
 package burnnote;
+use Modern::Perl;
 use Dancer2;
 use Dancer2::Plugin::DBIC;
 use Data::Printer;
+use Data::GUID 'guid_string';
+use Net::IP::Match::Regexp qw( create_iprange_regexp match_ip );
+
+
 
 our $VERSION = '0.1';
 
@@ -10,15 +15,40 @@ get '/' => sub {
 };
 
 post '/' => sub {
-    my $body = body_parameters->as_hashref;
-    p $body;
-    validate_input();
-    template 'index' => { 'title' => 'burnnote' };
+    my $url = request->base;
+
+    my $add = add_message({
+        message  => body_parameters->get('message'),
+        internal => ( body_parameters->get('internal') ? 1 : 0 ),
+        id       => guid_string(),
+        time     => time()
+    });
+
+    
+    $url .= $add->id;
+
+    template 'index' => { 'title' => 'burnnote', url => $url};
+};
+
+get '/:id' => sub {
+    my $id  = route_parameters->get('id');
+    my $row = get_row($id);
+}
+
+
+sub add_message {
+    my $params = shift;
+    my $rs = resultset('Note');
+    my $res = $rs->create( $params );
+    return $res;
+
 };
 
 
-sub validate_input {
-};
+sub get_row {
+    my $id = shift;
+    my $rs = resultset('Note')->find($id);
+}
 
 
 true;
